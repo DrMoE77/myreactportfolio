ff50a9515417043ae7534333361b436723327528
DrMoE77 <drmo.afifi@gmail.com>
2022-05-24 22:37:53 +1000

    about

---
+++ src/webpage/about/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function about (){
+    return (
+        <section>
+            <div className='' id = "about-div">
+
+</div>
+        </section>
+        
+    )
+
+}
